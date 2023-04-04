ecde7c3b7eda568391689bbf9fc2fa78ae380109
Gosend <bobbyjoseph108@gmail.com>
2023-04-04 10:01:46 +0100

    making the ui responsive

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Icon, StackProps, Text } from "@chakra-ui/react";
+import { Box, Flex, Stack, HStack, Icon, StackProps, Text } from "@chakra-ui/react";
 import React, { ElementType } from "react";
 import {
   HassleFreeIcon,
@@ -14,16 +14,18 @@ function Feature(props: FeatureProps) {
   const { icon, children, ...rest } = props;
   return (
     <HStack {...rest} spacing={"24px"}>
-      <Icon as={icon} boxSize={"48px"} />
-      <Text textAlign={'left'} fontSize={"18px"} fontWeight={'700'}>{children}</Text>
+      <Icon as={icon} boxSize={['32px', '32px', '48px']} />
+      <Text textAlign={"left"} fontSize={['12px', '12px', '18px']} fontWeight={"700"}>
+        {children}
+      </Text>
     </HStack>
   );
 }
 
 export function Features() {
   return (
-    <Box maxW={"1024px"} m={'auto'} pt={'60px'} pb={'35px'}>
-      <HStack px={'48px'} spacing={'20px'}>
+    <Box maxW={"1024px"} m={"auto"} pt={['30px', '30px', '60px']} pb={"35px"}>
+      <Stack direction={['column', 'column', 'row']} px={"48px"} spacing={"20px"}>
         <Feature icon={MoneyBackGuaranteeIcon}>
           30 days money back Guarantee
         </Feature>
@@ -33,7 +35,7 @@ export function Features() {
         <Feature icon={MonthlySubscriptionIcon}>
           No monthly subscription Pay once and for all
         </Feature>
-      </HStack>
+      </Stack>
     </Box>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,15 +3,22 @@ import React from "react";
 
 function Header() {
   return (
-    <Box pb={'112px'} as="section">
-    <Box textAlign={'center'} color={"#F7FAFc"} bg={"#6b46c1"} pt={"90px"} pb={"198px"} px={'32px'}>
-      <Heading fontWeight={"800"} fontSize={"42px"}>
-        Simple pricing for your business
-      </Heading>
-      <Text fontWeight={"500"} fontSize={"24px"} pt={'16px'}>
-        Plans that are carefully crafted to suit your business
-      </Text>
-    </Box>
+    <Box pb={"112px"} as="section">
+      <Box
+        textAlign={['left', 'left', 'center']}
+        color={"#F7FAFc"}
+        bg={"#6b46c1"}
+        pt={"90px"}
+        pb={"198px"}
+        px={"32px"}
+      >
+        <Heading fontWeight={"800"} fontSize={['3xl', '3xl', '5xl']}>
+          Simple pricing for your business
+        </Heading>
+        <Text fontWeight={"500"} fontSize={['lg', 'lg', '2xl']} pt={"16px"}>
+          Plans that are carefully crafted to suit your business
+        </Text>
+      </Box>
     </Box>
   );
 }
